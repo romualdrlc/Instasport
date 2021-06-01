3172939c3e0a99ccb1b2b4fb195f01a88d35d6ac
romuald <romuald.lecorroller@gmail.com>
2021-06-01 17:56:24 +0200

    gestion login a revoir

--- pages/api/oauth/[callbackWithParam].tsx
+++ pages/api/oauth/[callbackWithParam].tsx
@@ -2,63 +2,57 @@
 import React from "react";
 //import cookieCutter from "cookie-cutter";
 import { MongoClient } from "mongodb";
-import { useCookies } from "react-cookie"
-
-
-  
+import { useCookies } from "react-cookie";
+import UserProfile from "../../../components/userprofile";
 
 const AuthCallback: React.FC = (props: any) => {
   const [cookie, setCookie] = useCookies(["user"]);
 
-
-
   const handleSignIn = async () => {
     try {
-      const response =  props.token;
-setCookie("user", JSON.stringify(response), {
+      const response = props.token;
+      setCookie("user", JSON.stringify(response), {
         path: "/",
         maxAge: 3600, // Expires after 1hr
         sameSite: true,
-      })
+      });
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   handleSignIn();
 
-
   return (
     // <Layout>
-      <div className="container">
-        <div className="row">
-          <div className="col-3"></div>
-          <div className="col-6">
-            <h3>You're finally logged in !</h3>
-            <h5>Your access token is : {props.token}</h5>
-          </div>
-          <div className="col-3"></div>
-        </div>
-      </div>
+    // <div className="container">
+    //   <div className="row">
+    //     <div className="col-3"></div>
+    //     <div className="col-6">
+    //       <h3>You're finally logged in !</h3>
+    //       <h5>Your access token is : {props.token}</h5>
+    //     </div>
+    //     <div className="col-3"></div>
+    //   </div>
+    // </div>
     // </Layout>
+    <>
+      <UserProfile />
+      <h5>Your access token is : {props.token}</h5>
+    </>
   );
 };
 
 export default AuthCallback;
 
 export const getServerSideProps = async (context) => {
-
-
   const accessToken = context.query.code;
-    //
+  //
   //ex http://localhost:3000/oauth/callback?code=tTbzVbtqsymiibUTL_MGYw%3D%3D&state=
 
-console.log("----------URL------------", context.query.code);
+  console.log("----------URL------------", context.query.code);
 
   //cookieCutter.set('myGamesCookie', accessToken);
-
-
-
 
   const databaseUrl = process.env.MONGODB_URI;
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-
 //recupt access token de FL
 //l'inserer dans header cookie
 //afficher (griser le bon lien) sur toutes les pages
@@ -6,6 +5,7 @@
 //import Layout from "../components/layout";
 import React from "react";
 import { GetServerSideProps } from "next";
+import Head from "next/head";
 
 //require('dotenv').config();
 
@@ -16,10 +16,86 @@ import OAuth2Client, {
 const SignInWithFewLines: React.FC = (props: any) => {
   return (
     // <Layout>
-      <a href={props.url}>
-        <h3>Login with FL</h3>
-      </a>
-    // </Layout>
+
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+          integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
+          crossOrigin="anonymous"
+        />
+      </Head>
+
+      <div className="container">
+        <div className="text-center">
+          <img className="logo" src="logo.png" width="150" height="150" />
+        </div>
+        <div className="d-flex justify-content-center h-100">
+          <div className="card">
+            <div className="card-header">
+              <button type="button" className="btn btn-warning">
+                <a href={props.url}>Login Fewlines</a>
+              </button>
+            </div>
+            <div className="card-body">
+              <form>
+                <div className="input-group form-group">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text">
+                      <i className="fas fa-user" />
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="username"
+                  />
+                </div>
+                <div className="input-group form-group">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text">
+                      <i className="fas fa-key" />
+                    </span>
+                  </div>
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="password"
+                  />
+                </div>
+                <div className="row align-items-center remember">
+                  <input type="checkbox" />
+                  Remember Me
+                </div>
+                <div className="form-group">
+                  <input
+                    type="submit"
+                    defaultValue="Login"
+                    className="btn float-right login_btn"
+                  />
+                </div>
+              </form>
+            </div>
+            <div className="card-footer">
+              <div className="d-flex justify-content-center links">
+                Don't have an account?<a href="#">Sign Up</a>
+              </div>
+              <div className="d-flex justify-content-center">
+                <a href="#">Forgot your password?</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+    /* </Layout> */
   );
 };
 
@@ -31,7 +107,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
       "https://fewlines.connect.prod.fewlines.tech/.well-known/openid-configuration",
     clientID: process.env.CONNECT_CLIENT_ID,
     clientSecret: process.env.CONNECT_CLIENT_SECRET,
-    redirectURI: process.env.CONNECT_REDIRECT_URI + "/oauth/callback",
+    redirectURI: process.env.CONNECT_REDIRECT_URI,
     audience: "wdb2g1",
     scopes: ["openid", "email"],
   };
@@ -44,4 +120,3 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: { url: JSON.parse(JSON.stringify(urlToSignIn)) },
   };
 };
-
