da7433058fa475d6829f519b5b00aabfb39cb347
MohirM <mohir591@gmail.com>
2021-06-04 16:15:07 +0200

    style for login usernews and add css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,76 +1,24 @@
-
 import React from "react";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Layout from "../components/Layout";
 import initClient from "../utils/initClient";
 
-
-
 const SignInWithFewLines: React.FC = (props: any) => {
   return (
     <Layout>
       <>
-        <div className="container">
+        <div className="CardFullLogin text-center">
           <div className="text-center">
             <img className="logo" src="logocarre.png" />
           </div>
-          <div className="d-flex justify-content-center h-100">
-            <div className="card">
-              <div className="card-header">
-                <button type="button" className="btn btn-warning">
-                  <a href={props.url}>Login Fewlines</a>
-                </button>
-              </div>
-              <div className="card-body">
-                <form>
-                  <div className="input-group form-group">
-                    <div className="input-group-prepend">
-                      <span className="input-group-text">
-                        <i className="fas fa-user" />
-                      </span>
-                    </div>
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="username"
-                    />
-                  </div>
-                  <div className="input-group form-group">
-                    <div className="input-group-prepend">
-                      <span className="input-group-text">
-                        <i className="fas fa-key" />
-                      </span>
-                    </div>
-                    <input
-                      type="password"
-                      className="form-control"
-                      placeholder="password"
-                    />
-                  </div>
-                  <div className="row align-items-center remember">
-                    <input type="checkbox" />
-                    Remember Me
-                  </div>
-                  <div className="form-group">
-                    <input
-                      type="submit"
-                      defaultValue="Login"
-                      className="btn float-right login_btn"
-                    />
-                  </div>
-                </form>
-              </div>
-              <div className="card-footer">
-                <div className="d-flex justify-content-center links">
-                  Don't have an account?<a href="#">Sign Up</a>
-                </div>
-                <div className="d-flex justify-content-center">
-                  <a href="#">Forgot your password?</a>
-                </div>
-              </div>
-            </div>
-          </div>
+          <h4 style={{ color: "#FBE23B" }}>Login with :</h4>
+          <br />
+          <button type="button" className="btn btn-warning">
+            <a style={{ color: "black" }} href={props.url}>
+              Fewlines
+            </a>
+          </button>
         </div>
       </>
     </Layout>
@@ -80,7 +28,6 @@ const SignInWithFewLines: React.FC = (props: any) => {
 export default SignInWithFewLines;
 
 export const getServerSideProps: GetServerSideProps = async () => {
-
   const urlToSignIn = await initClient().getAuthorizationURL();
 
   return {
--- pages/usersnews.tsx
+++ pages/usersnews.tsx
@@ -1,6 +1,194 @@
+import { NextPage, GetServerSideProps } from "next";
 import React from "react";
+import { getDatabase } from "../util/mongodb";
 
-const usersNews: React.FC = (props: any) => {
-  return <p>fil actualité</p>;
+const newsletter: NextPage<{ data; user }> = ({ data, user }) => {
+  return (
+    <div>
+      <div className="BodyNews">
+        <div className="container">
+          <div className="row">
+            <div className="Nav">
+              <nav className="row">
+                <div className="itemNavbar col-3">
+                  <img className="logoNav" src="../logocarre.png" />
+                </div>
+                <div className="itemNavbar col-6">
+                  <div className="SearchBar d-flex">
+                    <input
+                      className="form-control me-2"
+                      type="search"
+                      placeholder="Search"
+                      aria-label="Search"
+                    />
+                  </div>
+                </div>
+                <div className="itemNavbar col-3">
+                  <img
+                    className="photoNav"
+                    key={user[0].id}
+                    src={user[0].photo}
+                    alt=""
+                  />
+                </div>
+              </nav>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="BodyNews col-3">
+            <div className="container">
+              {user.map((value) => {
+                return (
+                  <div className="DivSugg">
+                    <div>
+                      <img
+                        key={value.id}
+                        className="SuggestionProfil"
+                        src={value.photo}
+                        alt=""
+                      />
+                    </div>
+                    <button className="btn btn-secondary">
+                      Add {value.UserName}
+                    </button>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="BodyNews col-5"></div>
+          <div className="BodyNews col-4">
+            <div className="container">
+              <div>
+                <div className="container">
+                  <div className="row">
+                    <div className="">
+                      <div className="comment mt-4 text-justify float-left">
+                        {" "}
+                        <img
+                          src="https://i.imgur.com/yTFUilP.jpg"
+                          alt=""
+                          className="rounded-circle"
+                          width={40}
+                          height={40}
+                        />
+                        <h4>Jhon Doe</h4> <span>- 20 October, 2018</span> <br />
+                        <p>
+                          Lorem ipsum dolor sit, amet consectetur adipisicing
+                          elit. Accusamus numquam assumenda hic aliquam vero
+                          sequi velit molestias doloremque molestiae dicta?
+                        </p>
+                      </div>
+                      <div className="text-justify darker mt-4 float-right">
+                        {" "}
+                        <img
+                          src="https://i.imgur.com/CFpa3nK.jpg"
+                          alt=""
+                          className="rounded-circle"
+                          width={40}
+                          height={40}
+                        />
+                        <h4>Rob Simpson</h4> <span>- 20 October, 2018</span>{" "}
+                        <br />
+                        <p>
+                          Lorem ipsum dolor sit, amet consectetur adipisicing
+                          elit. Accusamus numquam assumenda hic aliquam vero
+                          sequi velit molestias doloremque molestiae dicta?
+                        </p>
+                      </div>
+                      <div className="comment mt-4 text-justify">
+                        {" "}
+                        <img
+                          src="https://i.imgur.com/yTFUilP.jpg"
+                          alt=""
+                          className="rounded-circle"
+                          width={40}
+                          height={40}
+                        />
+                        <h4>Jhon Doe</h4> <span>- 20 October, 2018</span> <br />
+                        <p>
+                          Lorem ipsum dolor sit, amet consectetur adipisicing
+                          elit. Accusamus numquam assumenda hic aliquam vero
+                          sequi velit molestias doloremque molestiae dicta?
+                        </p>
+                      </div>
+                      <div className="darker mt-4 text-justify">
+                        {" "}
+                        <img
+                          src="https://i.imgur.com/CFpa3nK.jpg"
+                          alt=""
+                          className="rounded-circle"
+                          width={40}
+                          height={40}
+                        />
+                        <h4>Rob Simpson</h4> <span>- 20 October, 2018</span>{" "}
+                        <br />
+                        <p>
+                          Lorem ipsum dolor sit, amet consectetur adipisicing
+                          elit. Accusamus numquam assumenda hic aliquam vero
+                          sequi velit molestias doloremque molestiae dicta?
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <form id="algin-form">
+                <div className="form-group">
+                  <h4>Leave a comment</h4>
+                  <label htmlFor="message">Message</label>
+                  <textarea
+                    name="msg"
+                    cols={30}
+                    rows={5}
+                    className="form-control"
+                    style={{ backgroundColor: "white" }}
+                    defaultValue={""}
+                  />
+                </div>
+                <div className="form-group">
+                  <button type="button" id="post" className="btn">
+                    Post Comment
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
-export default usersNews;
+export default newsletter;
+export const getServerSideProps: GetServerSideProps = async () => {
+  const mongodb = await getDatabase();
+  const categoSport = await mongodb.db().collection("posts").find().toArray();
+  const UserData = await mongodb.db().collection("user").find().toArray();
+  const result = await categoSport.map((value) => {
+    return {
+      id: value.id,
+      userId: value.userId,
+      datePost: value.datePost,
+      photosPost: value.photosPost,
+      textPost: value.textPost,
+      likePost: value.likePost,
+      commentsPost: value.commentsPost,
+    };
+  });
+  const result2 = await UserData.map((value) => {
+    return {
+      id: value.Id,
+      UserName: value.UserName,
+      photo: value.Cover,
+    };
+  });
+  const fin = await JSON.parse(JSON.stringify(result));
+  const fin2 = await JSON.parse(JSON.stringify(result2));
+  return {
+    props: {
+      data: fin,
+      user: fin2,
+    },
+  };
+};
