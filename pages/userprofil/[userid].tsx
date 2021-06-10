d722ab611e816d2d906b06ab20e28817d360dcc2
romuald <romuald.lecorroller@gmail.com>
2021-06-10 23:41:09 +0200

    fincition front et page atterissage search

--- pages/userprofil/[userid].tsx
+++ pages/userprofil/[userid].tsx
@@ -2,124 +2,176 @@ import React, { useState, useEffect } from "react";
 import { NextPage, GetServerSideProps } from "next";
 import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
+import Checkbox from "../../components/checkBox";
 
 const UserProfil: NextPage = (props: any) => {
-  const [userName, setUserInfo] = useState();
+  const [userInfo, setUserInfo] = useState(null);
+  // const [mySports, setMySports] = useState([]);
+
+  const categoriesImgDescription = [
+    "Football",
+    "BasketBall",
+    "Ping-pong",
+    "Tennis",
+    "Rugby",
+    "Swimming",
+    "Volley-Ball",
+    "Badminton",
+    "Baseball",
+  ];
+  // if (userInfo) {
+  //   userInfo.Groups.filter((group, index) => group[index]);
+  // }
 
   useEffect(() => {
     searchUserInDB();
-  }, []);
+  }, [userInfo]);
 
   const searchUserInDB = async () => {
     const user = fetch("/api/searchbyid?searchId=" + props.userId)
       .then((res) => res.json())
       .then((user) => {
         console.log("$$$$$$$$$$$$$$$$$$$$$$$$$", user);
-        setUserInfo(user.userName);
+        setUserInfo(user);
       });
   };
   return (
-    <>
-      <Layout>
-        <div className="container">
-          <p>{userName}</p>
-          {/* <p>{userInfo[0].email}</p> */}
-        </div>
-        {/* <div className="bodyPost">
-      <div className="BodyNews">
-        {/* <div className="row">
-          <div className="Nav">
-            <nav className="navbar row">
-              <div className="itemNavbar col-3"></div>
-              <div className="profilPhoto col-6">
-                <div className="profilImage">
+    <Layout>
+      <div className="container " style={{ paddingTop: 200 }}>
+        <div className="page-inscription">
+          <div className="container profilePhotoEdit">
+            <div className="row">
+              <div className="col-5"></div>
+              <div className="col-2">
+                <div className="card" style={{ width: 200, height: 200 }}>
                   <img
-                    className="photoNav"
-                    key={user[0].id}
-                    src={user[0].photo}
-                    alt=""
+                    className="image-profil"
+                    src={
+                      userInfo
+                        ? userInfo.Cover
+                        : "https://images.vexels.com/media/users/3/136558/isolated/lists/43cc80b4c098e43a988c535eaba42c53-person-user-icon.png"
+                    }
                   />
                 </div>
               </div>
-              <div className="itemNavbar col-3">
-                <a href="/">Logout</a>
-              </div>
-            </nav>
-          </div>
-        </div> *
-        <div className="row">
-          <div className="BodyNews col-3">
-            <div className="container">
-              {errorMessage ? <p>{errorMessage}</p> : <></>}
-              {user.map((value, index) => {
-                return (
-                  <div className="DivSugg" key={index}>
-                    <div>
-                      <img
-                        key={value.id}
-                        className="SuggestionProfil"
-                        src={value.photo}
-                        alt=""
-                      />
-                    </div>
-                    <button className="btn btn-secondary">
-                      Add {value.UserName}
-                    </button>
-                  </div>
-                );
-              })}
             </div>
+            <div className="col-5"></div>
           </div>
-          <div className="BodyNews col-5">
-            
-            <button onClick={() => setIsCommentVisible(!isCommentVisible)}>
-              comment
-            </button>
-            {console.log(isCommentVisible)}
-          </div>
-          <div className="BodyNews col-4">
-            <div className="container">
-              <div>
+        </div>
+        <div className="container">
+          <div>
+            <div className="row row-cols-1 row-cols-sm-2">
+              <div className="colIncription col">
+                <label htmlFor="exampleInputUserName" className="form-label">
+                  UserName
+                </label>
+                <input
+                  type="text"
+                  disabled
+                  className="form-control"
+                  id="exampleInputEmail"
+                  placeholder={userInfo ? userInfo.userName : null}
+                />
+                <label htmlFor="exampleInputUserName" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="text"
+                  disabled
+                  className="form-control"
+                  id="exampleInputEmail"
+                  placeholder={userInfo ? userInfo.email : null}
+                />
+                <label htmlFor="exampleInputBirthDate" className="form-label">
+                  Birthdate
+                </label>
+                <input
+                  type="text"
+                  name="trip-start"
+                  min="1921-01-01"
+                  max="2003-12-31"
+                  className="form-control"
+                  id="exampleInputBirthDate"
+                  aria-describedby="emailHelp"
+                  placeholder={userInfo ? userInfo.Birthdate : null}
+                />
+              </div>
+              <div className="col">
+                <h6
+                  className="titre-interests text-center"
+                  style={{ marginTop: 120 }}
+                >
+                  Favorite sports :
+                </h6>
                 <div className="container">
-                  <div className="row">
-                    <div className="comment-card">
-                      {isCommentVisible ? <CommentPost data={data} /> : null}
-                    </div>
+                  <div className="row row-cols-3 ">
+                    <ul style={{ marginLeft: 250 }}>
+                      {
+                        userInfo && userInfo.Groups
+                          ? userInfo.Groups.map((value, index) => {
+                              if (value) {
+                                return (
+                                  <div>
+                                    <li>
+                                      <p>{categoriesImgDescription[index]}</p>
+                                    </li>
+                                  </div>
+                                );
+                              }
+                            })
+                          : null
+
+                        //   return (
+                        //     <div
+                        //       className="imageInterest col text-center"
+                        //       key={index}
+                        //     >
+                        //       <img
+                        //         className="imageCircle"
+                        //         src={imageOfCategory}
+                        //         width="70"
+                        //         height="70"
+                        //         // title={categoriesImgDescription[index]}
+                        //       />
+                        //       <Checkbox id={index} active={userInfo.Groups} />
+                        //     </div>
+                        //   );
+                      }
+                    </ul>
                   </div>
                 </div>
               </div>
-              <div onSubmit={(e) => handleSubmit(e)} id="algin-form">
-                <div className="form-group">
-                  <h4>Leave a comment</h4>
-                  <label htmlFor="message">Message</label>
-                  <textarea
-                    name="msg"
-                    cols={30}
-                    rows={5}
-                    onChange={handleChange}
-                    className="form-control"
-                    style={{ backgroundColor: "white" }}
-                    defaultValue={""}
-                  />
+              {/* <div className="text-center">
+                <div>
+                  {active.map((value, index) => {
+                    return <div key={"tata" + index}>{value}</div>;
+                  })}
                 </div>
-                <div className="form-group">
-                  <button
-                    type="submit"
-                    id="post"
-                    onClick={() => postComment()}
-                    className="btn"
-                  >
-                    Post Comment
-                  </button>
-                </div>
-              </div>
+                <button
+                  type="submit"
+                  className="Boutton btn bouton-update-profil"
+                  onClick={() => registerform()}
+                  // disabled={
+                  //   counterOfSelectedCategories < 3 ||
+                  //   userName === "" ||
+                  //   birthdate === "" ||
+                  //   usersEmail === ""
+                  // }
+                >
+                  Update
+                </button>
+              </div> */}
             </div>
           </div>
+          <style jsx>{`
+            .form-check-input {
+              background-color: #fbe23b;
+              color: black;
+            }
+          `}</style>
         </div>
       </div>
-            </div> */}
-      </Layout>
-    </>
+    </Layout>
   );
 };
 
