1f39f3063c9483dd5679ca140f201dc4863f1ed1
romuald <romuald.lecorroller@gmail.com>
2021-06-10 14:47:09 +0200

    modif vercel

--- pages/api/createcomment.ts
+++ pages/api/createcomment.ts
@@ -3,10 +3,4 @@ import { createComment } from "../../utils/initDatabase";
 
 export default async (request: NextApiRequest, response: NextApiResponse) => {
   const mongoResponse = await createComment(request.body);
-  console.log(mongoResponse);
-  if (mongoResponse.insertedCount === 1) {
-    response.json({ message: "register OK" });
-  } else {
-    response.json({ message: "ERROR" });
-  }
 };
